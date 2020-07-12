7a7ce54f240e70eab4687a90da1261418b6ec9ea
M0rp_ <markhazen05@hotmail.com>
2020-07-12 00:12:53 -0500

    push before adding virtual robot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Copy.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class Copy extends OpMode
+{
+
+    //DcMotor test = null;
+
+    @Override
+    public void init ()
+    {
+
+        //test = hardwareMap.dcMotor.get("test");
+
+        //test.setPower(0);
+
+    }
+
+    @Override
+    public void start ()
+    {
+
+        //test.setPower(0);
+
+    }
+
+    @Override
+    public void loop ()
+    {
+
+        //test.setPower(-gamepad1.left_stick_y);
+
+    }
+
+    @Override
+    public void stop ()
+    {
+
+        //test.setPower(0);
+
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearOpModeEx.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class LinearOpModeEx extends LinearOpMode
+{
+
+
+
+    @Override
+    public void runOpMode ()
+    {
+
+
+
+        waitForStart();
+
+
+
+        while (opModeIsActive())
+        {
+
+
+
+        }
+
+    }
+
+}
